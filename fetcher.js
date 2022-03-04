378336be7584d52a971c8e28cbff23500472528d
Rajin <rajink92@gmail.com>
2022-03-03 19:01:28 -0500

    Add const and stdin and out

---
+++ fetcher.js
@@ -0,0 +1,7 @@
+const request = require('request');
+const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
