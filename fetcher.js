c71e9540a81973675e10efb6a0e0141039c2d193
Rajin <rajink92@gmail.com>
2022-03-04 13:38:49 -0500

    http request

--- fetcher.js
+++ fetcher.js
@@ -5,3 +5,22 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+const getResource = (serverAddress, localPath) => {
+  request(serverAddress, (error, response, body) => {
+    if (error) {
+      console.log(error);
+      process.exit();
+    }
+    fs.writeFile(localPath, body, (error) => 
+      fs.access(localPath, (error) => {
+        if (error) console.log(error);
+        process.exit();
+      });
+      if (error) console.log(error);
+      console.log(`Downloaded and saved ${body.length} byte to ${localPath}`);
+      rl.close();
+    });
+  });
+};
+
